152fde6b8f50f66e46cf466bcfb67b3eb81c0fc2
Darshit Shah <darnir@gmail.com>
2016-05-11 09:47:50 +0200

    Script containing common functions for all others

---
+++ include.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rtems_init() {
+	if [[ ${RTEMS_ACTIVE:-0} -ne 1 ]]; then
+		echo "Please set up the RTEMS dev env via the rtems command first"
+		exit 1
+	else
+		pushd "$HOME/Programming/rtems"
+	fi
+
+	export RTEMS_LOG_DIR="$HOME/Programming/rtems/builds/logs"
+	mkdir -p "$RTEMS_LOG_DIR"
+}
